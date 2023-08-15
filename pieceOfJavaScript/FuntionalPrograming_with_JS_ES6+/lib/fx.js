44cd7fc106eea88e73eb6ff3d43cc5e980d6aff8
안강민 <hkkm612@gmail.com>
2023-08-15 13:51:18 +0900

    update. [JavaScript / 함수형 프로그래밍과 JS ES6+] Section 4. 코드를 값으로 다루어 표현력 높이기 (중간 정리)
    - go(실행), pipe(정의), curry(정의) 함수들을 바탕으로 함수의 표현을 더 좋게(간결하고, 이해하기 쉽게) 할 수 있다.

--- pieceOfJavaScript/FuntionalPrograming_with_JS_ES6+/lib/fx.js
+++ pieceOfJavaScript/FuntionalPrograming_with_JS_ES6+/lib/fx.js
@@ -1,24 +1,36 @@
-const map = (f, iter) => {
+const curry =
+    (f) =>
+        (a, ..._) =>
+            _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+const map = curry((f, iter) => {
     let res = [];
     for (const a of iter) {
         res.push(f(a));
     }
     return res;
-};
+});
 
-const filter = (f, iter) => {
+const filter = curry((f, iter) => {
     let res = [];
     for (const a of iter) {
         if (f(a)) res.push(a);
     }
     return res;
-};
+});
 
-const reduce = (f, acc, iter) => {
+const reduce = curry((f, acc, iter) => {
     if (!iter) {
         iter = acc[Symbol.iterator]();
         acc = iter.next().value;
     }
     for (const a of iter) acc = f(acc, a);
     return acc;
-};
+});
+
+const go = (...args) => reduce((a, f) => f(a), args);
+
+const pipe =
+    (f, ...fs) =>
+        (...as) =>
+            go(f(...as), ...fs);
