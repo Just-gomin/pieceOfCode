2266657ca3a00a7aed797105736abab3e20909da
안강민 <hkkm612@gmail.com>
2023-08-16 15:50:02 +0900

    update. [JavaScript / 함수형 프로그래밍과 JS ES6+] lib/fx.js log, range, take 함수 추가

--- pieceOfJavaScript/FuntionalPrograming_with_JS_ES6+/lib/fx.js
+++ pieceOfJavaScript/FuntionalPrograming_with_JS_ES6+/lib/fx.js
@@ -1,8 +1,12 @@
+const log = console.log;
+
 // 함수를 인자로 받아 인자가 두 개 일때 까지 대기 하는 함수를 만드는 함수
 const curry =
     (f) =>
         (a, ..._) =>
             _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+
 
 // 함수 f와 이터러블을 받아, 이터러블 각각에 대해 함수 f를 반복해 수행하는 함수
 const map = curry((f, iter) => {
@@ -40,3 +44,36 @@ const pipe =
     (f, ...fs) =>
         (...as) =>
             go(f(...as), ...fs);
+
+// 0 ~ l-1 까지 숫자가 담긴 배열을 생성
+const range = (l) => {
+    let i = -1;
+    let res = [];
+    while (++i < l) {
+        res.push(i);
+    }
+
+    return res;
+};
+
+// iterable을 받아 l개 만큼 뽑아내 반환
+const take = curry((l, iter) => {
+    let res = [];
+    for (const a of iter) {
+        res.push(a);
+        if (res.length === l) return res;
+    }
+    return res;
+});
+
+// ---------------
+//  Lazy
+// ---------------
+const L = {};
+L.range = function* (l) {
+    let i = -1;
+    while (++i < l) {
+        //   log(`${i},L.range`);
+        yield i;
+    }
+};
