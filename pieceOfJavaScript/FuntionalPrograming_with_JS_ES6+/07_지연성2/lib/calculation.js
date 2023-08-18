fbb081de5694f523ec9f2a9801f06dc3e658d666
안강민 <hkkm612@gmail.com>
2023-08-18 18:19:59 +0900

    update. [JavaScript / 함수형 프로그래밍과 JS ES6+] Section 7. 지연성 2 (중간정리)
    - map & filter 함수 수정 위해 lib/fx.js 복제, 기존 map & filter를 L.map & L.filter를 이용해 변경

---
+++ pieceOfJavaScript/FuntionalPrograming_with_JS_ES6+/07_지연성2/lib/calculation.js
@@ -0,0 +1,4 @@
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
---
+++ pieceOfJavaScript/FuntionalPrograming_with_JS_ES6+/07_지연성2/lib/fx.js
@@ -0,0 +1,61 @@
+const log = console.log;
+
+// 함수를 인자로 받아 인자가 두 개 일때 까지 대기 하는 함수를 만드는 함수
+const curry =
+    (f) =>
+        (a, ..._) =>
+            _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+// 이터러블과 함수들을 받아 이터러블에 대해 순차 적으로 함수들을 수행하는 함수
+const go = (...args) => reduce((a, f) => f(a), args);
+
+// 함수들을 받아 순차 적으로 함수들을 수행하는 함수를 정의하는 함수
+const pipe =
+    (f, ...fs) =>
+        (...as) =>
+            go(f(...as), ...fs);
+
+
+// 함수 f와 누계, 이터러블을 받아, 이터러블 각각에 대해 함수 f를 수행하고 누적하는 함수
+const reduce = curry((f, acc, iter) => {
+    if (!iter) {
+        iter = acc[Symbol.iterator]();
+        acc = iter.next().value;
+    }
+    for (const a of iter) acc = f(acc, a);
+    return acc;
+});
+
+// 0 ~ l-1 까지 숫자가 담긴 배열을 생성
+const range = (l) => {
+    let i = -1;
+    let res = [];
+    while (++i < l) {
+        res.push(i);
+    }
+
+    return res;
+};
+
+// iterable을 받아 l개 만큼 뽑아내 반환
+const take = curry((l, iter) => {
+    let res = [];
+    for (const a of iter) {
+        res.push(a);
+        if (res.length === l) return res;
+    }
+    return res;
+});
+
+// ---------------
+//  Lazy
+// ---------------
+const L = {};
+L.range = function* (l) {
+    let i = -1;
+    while (++i < l) {
+        //   log(`${i},L.range`);
+        yield i;
+    }
+};
+
