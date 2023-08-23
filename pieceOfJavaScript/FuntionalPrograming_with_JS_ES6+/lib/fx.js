c7a7aec845c805e9bc52f7d1fe41338b261fb65f
안강민 <hkkm612@gmail.com>
2023-08-23 18:22:56 +0900

    create. [JavaScript / 함수형 프로그래밍과 JS ES6+] Section 9. 비동기:동시성 프로그래밍2(중간정리)
    - Promise 처리 가능하게 함수 수정

--- pieceOfJavaScript/FuntionalPrograming_with_JS_ES6+/lib/fx.js
+++ pieceOfJavaScript/FuntionalPrograming_with_JS_ES6+/lib/fx.js
@@ -1,5 +1,7 @@
 const log = console.log;
 
+const add = (a, b) => a + b;
+
 // 함수를 인자로 받아 인자가 두 개 일때 까지 대기 하는 함수를 만드는 함수
 const curry =
     (f) =>
@@ -21,11 +23,21 @@ const pipe =
 // iterable을 받아 l개 만큼 뽑아내 반환
 const take = curry((l, iter) => {
     let res = [];
-    for (const a of iter) {
-        res.push(a);
-        if (res.length === l) return res;
-    }
-    return res;
+    iter = iter[Symbol.iterator]();
+
+    return function recur() {
+        while (!(cur = iter.next()).done) {
+            const a = cur.value;
+            if (a instanceof Promise) {
+                return a
+                    .then((a) => (res.push(a), res).length === l ? res : recur())
+                    .catch((e) => e === nop ? recur() : Promise.reject(e));
+            }
+            res.push(a);
+            if (res.length === l) return res;
+        }
+        return res;
+    }();
 });
 
 // 전부 다 반환
@@ -33,6 +45,8 @@ const takeAll = take(Infinity);
 
 // 이터러블인지 확인
 const isIterable = (a) => a && a[Symbol.iterator];
+
+const nop = Symbol('nop');
 
 // ---------------
 //  Lazy
@@ -48,13 +62,15 @@ L.range = function* (l) {
 
 L.map = curry(function* (f, iter) {
     for (const a of iter) {
-        yield f(a);
+        yield go1(a, f);
     }
 });
 
 L.filter = curry(function* (f, iter) {
     for (const a of iter) {
-        if (f(a)) yield a;
+        const b = go1(a, f);
+        if (b instanceof Promise) yield b.then((b) => b ? a : Promise.reject(nop));
+        else if (b) yield a;
     }
 });
 
@@ -91,15 +107,26 @@ const map = curry(pipe(L.map, takeAll));
 // 함수 f와 이터러블을 받아, 이터러블 각각에 대해 함수 f를 반복해 조건을 만족하는 데이터를 추출하는 함수
 const filter = curry(pipe(L.filter, takeAll));
 
+const reduceF = (acc, a, f) =>
+    a instanceof Promise ?
+        a.then((a) => f(acc, a),
+            e => e === nop ? acc : Promise.reject(e)
+        )
+        : f(acc, a);
+
+const head = (iter) => go1(take(1, iter), ([h]) => {
+    return h
+});
+
 // 함수 f와 누계, 이터러블을 받아, 이터러블 각각에 대해 함수 f를 수행하고 누적하는 함수
 const reduce = curry((f, acc, iter) => {
-    if (!iter) {
-        iter = acc[Symbol.iterator]();
-        acc = iter.next().value;
-    }
+    if (!iter) return reduce(f, head(iter = acc[Symbol.iterator]()), iter);
+
+    iter = iter[Symbol.iterator]();
+    let cur;
     return go1(acc, function recur(acc) {
-        for (const a of iter) {
-            acc = f(acc, a);
+        while (!(cur = iter.next()).done) {
+            acc = reduceF(acc, cur.value, f);
             if (acc instanceof Promise) return acc.then(recur);
         }
         return acc;
