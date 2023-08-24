a0412346c0328dc66c3b1f00f873ed5c6ac303ef
안강민 <hkkm612@gmail.com>
2023-08-24 22:48:45 +0900

    create. [TypeScript / NestJS] cats resource 추가

---
+++ pieceOfTypeScript/piece-of-nest-js/src/cats/cats.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+
+@Injectable()
+export class CatsService {
+  create(createCatDto: CreateCatDto) {
+    console.log(createCatDto);
+    return 'This action adds a new cat';
+  }
+
+  findAll() {
+    return `This action returns all cats`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} cat`;
+  }
+
+  update(id: number, updateCatDto: UpdateCatDto) {
+    console.log(updateCatDto);
+    return `This action updates a #${id} cat`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} cat`;
+  }
+}
