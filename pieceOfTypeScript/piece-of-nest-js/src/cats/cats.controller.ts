f9cb3d0bdbfbf3d7021683a5177818c32f55b1db
안강민 <hkkm612@gmail.com>
2023-08-28 13:51:08 +0900

    update. [TypeScript / NestJS] Controller 공부하며 주석 작성

--- pieceOfTypeScript/piece-of-nest-js/src/cats/cats.controller.ts
+++ pieceOfTypeScript/piece-of-nest-js/src/cats/cats.controller.ts
@@ -6,26 +6,43 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  HttpCode,
+  Header,
+  Redirect,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Request, Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
-  @Post()
+  @Get('ab*cd') // Route 와일드 카드 => '/cats/ab*cd' 처리
+  @Redirect('https://nestjs.com', 301) // Status Code와 함께 Redirection
+  wildCard() {
+    return 'This route uses a wildcard and redirect to https://nestjs.com.';
+  }
+
+  @Post() // "/cats" POST HTTP 요청의 endpoint
+  @HttpCode(204) // Overrides default status code for the decorated request method.
+  @Header('Cache-Control', 'none') // Specify a custom response header
   create(@Body() createCatDto: CreateCatDto) {
     return this.catsService.create(createCatDto);
   }
 
-  @Get()
-  findAll() {
+  @Get() // "/cats" GET HTTP 요청의 endpoint
+  findAll(@Req() request: Request) {
+    // @Req 데코레이터를 이용해 요청 객체에 접근할 수 있다.
+    console.log(request.headers);
     return this.catsService.findAll();
   }
 
-  @Get(':id')
+  @Get(':id') // '/cats/[CatID]' 경로 요청 시 처리. id 값 처리
   findOne(@Param('id') id: string) {
     return this.catsService.findOne(+id);
   }
@@ -38,5 +55,11 @@ export class CatsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.catsService.remove(+id);
+  }
+
+  @Get('breed/:id') // '/cats/breed" Get HTTP 요청의 endpoint
+  breed(@Param('id') id: string, @Res() res: Response) {
+    res.status(HttpStatus.OK).send(`Breeding cat.(id: ${id})`);
+    return 'breed...';
   }
 }
--- pieceOfTypeScript/piece-of-nest-js/src/cats/cats.module.ts
+++ pieceOfTypeScript/piece-of-nest-js/src/cats/cats.module.ts
@@ -3,7 +3,7 @@ import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
 
 @Module({
-  controllers: [CatsController],
-  providers: [CatsService],
+  controllers: [CatsController], // 해당 모듈에서 사용하는 Controller들 명시
+  providers: [CatsService], // Controller에서 사용하는 Provider들을 명시 => Service, Repositories, Factories, Helpers, and so on.
 })
 export class CatsModule {}
