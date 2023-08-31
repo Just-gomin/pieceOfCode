f178ddfecc96861955b76bf8b5b41439dc8eff39
안강민 <hkkm612@gmail.com>
2023-08-31 17:03:18 +0900

    update. [TypeScript / TypeScriptChain] crypto 패키지 추가 위한 작업 중

--- pieceOfTypeScript/typechain/src/index.ts
+++ pieceOfTypeScript/typechain/src/index.ts
@@ -1,1 +1,23 @@
-console.log("hi");
+import crypto from "crypto";
+
+interface BlockInterface {
+  prevHash: string;
+  height: number;
+  data: string;
+  hash: string;
+}
+
+class Block implements BlockInterface {
+  hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string): string {
+    return `${prevHash}${height}${data}`;
+  }
+}
