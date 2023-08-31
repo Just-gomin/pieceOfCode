8df66bad7108d7330eee95788ab8acb5ac560125
안강민 <hkkm612@gmail.com>
2023-08-31 17:23:40 +0900

    update. [TypeScript / TypeScriptChain] 단순한 blockChain Sample 작성

--- pieceOfTypeScript/typechain/src/index.ts
+++ pieceOfTypeScript/typechain/src/index.ts
@@ -22,3 +22,38 @@ class Block implements BlockInterface {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    return this.blocks.length === 0
+      ? ""
+      : this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const newBlock = new Block(this.getPrevHash(), this.blocks.length, data);
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks() {
+    //   return this.blocks;
+    return [...this.blocks]; // 임의로 외부에서 조작하는 것을 막기위해 새로운 배열을 반환
+  }
+}
+
+const blockChain = new BlockChain();
+
+blockChain.addBlock("1");
+blockChain.addBlock("2");
+blockChain.addBlock("3");
+
+let blocks = blockChain.getBlocks();
+blocks.push(new Block(blocks[blocks.length - 1].hash, blocks.length, "4"));
+
+console.log(blockChain.getBlocks());
+console.log(blocks);
