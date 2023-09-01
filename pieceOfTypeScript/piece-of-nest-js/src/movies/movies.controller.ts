cbdffddf398d6e4d7afbcc3b1c4a9d1eb8261cce
안강민 <hkkm612@gmail.com>
2023-09-01 17:11:07 +0900

    update. [TypeScript / NestJS] movies controller 메서드 데코레이터 및 parameter, query 이용 방법 추가

--- pieceOfTypeScript/piece-of-nest-js/src/movies/movies.controller.ts
+++ pieceOfTypeScript/piece-of-nest-js/src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,14 +16,23 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
+  @Get('search')
+  search(@Query('title') title?: string, @Query('year') year?: string) {
+    let result: string = 'We are searching for a movie with a';
+    if (title) result += ` ${title}`;
+    if (year) result += `, ${year}`;
+    result += '.';
+    return result;
+  }
+
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `This will return specific movie. ID: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return `This will create movie.`;
+  create(@Body() movieData) {
+    return movieData;
   }
 
   @Delete('/:id')
@@ -23,7 +41,7 @@ export class MoviesController {
   }
 
   @Patch('/:id')
-  update(@Param('id') movieId: string) {
-    return `This will update a movie. ID: ${movieId}`;
+  update(@Param('id') movieId: string, @Body() updateData) {
+    return updateData;
   }
 }
