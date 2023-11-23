548daec46072a12cec1144d25fc9810e3c321d91
안강민 <hkkm612@gmail.com>
2023-11-23 15:00:30 +0900

    create. [Flutter / actual_flutter] 플러터 실전 강의 수강 디렉터리 생성

---
+++ pieceOfFlutter/actual_flutter/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
