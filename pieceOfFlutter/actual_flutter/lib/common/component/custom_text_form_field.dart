9a6254d334e12320739041b0c3b31e8aaf80ac75
안강민 <hkkm612@gmail.com>
2023-11-23 20:15:19 +0900

    update. [actual_flutter/common/component] CustomTextFormField 기본 보더 연하게 설정

--- pieceOfFlutter/actual_flutter/lib/common/component/custom_text_form_field.dart
+++ pieceOfFlutter/actual_flutter/lib/common/component/custom_text_form_field.dart
@@ -42,6 +42,7 @@ class CustomTextFormField extends StatelessWidget {
         fillColor: INPUT_BG_COLOR,
         filled: true,
         border: baseBorder,
+        enabledBorder: baseBorder,
         focusedBorder: baseBorder.copyWith(
           borderSide: baseBorder.borderSide.copyWith(
             color: PRIMARY_COLOR,
