7553ca0581fff88832a1ac91e2e1aea3035574f4
안강민 <hkkm612@gmail.com>
2023-11-24 18:35:45 +0900

    update. [actual_flutter/User/View] Login 화면 HTTP 요청 로직 추가

--- pieceOfFlutter/actual_flutter/lib/user/view/login_screen.dart
+++ pieceOfFlutter/actual_flutter/lib/user/view/login_screen.dart
@@ -1,13 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:actual_flutter/common/component/custom_text_form_field.dart';
 import 'package:actual_flutter/common/const/colors.dart';
 import 'package:actual_flutter/common/layout/default_layout.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = "";
+  String password = "";
+
+  @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+
+    // localhost
+    const emulatorIP = '10.0.0.2:3000';
+    const simulatorIP = '127.0.0.1:3000';
+
+    final ip = Platform.isAndroid ? emulatorIP : simulatorIP;
+
     return DefaultLayout(
       child: SingleChildScrollView(
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
@@ -29,16 +49,43 @@ class LoginScreen extends StatelessWidget {
                 const SizedBox(height: 16.0),
                 CustomTextFormField(
                   hintText: "이메일을 입력해주세요",
-                  onChanged: (String value) {},
+                  obscureText: false,
+                  onChanged: (String value) {
+                    username = value;
+                  },
                 ),
                 const SizedBox(height: 16.0),
                 CustomTextFormField(
                   hintText: "비밀번호를 입력해주세요",
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    password = value;
+                  },
                 ),
                 const SizedBox(height: 16.0),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    // ID:비밀번호 문자열
+                    String rawString = '$username:$password';
+
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+                    String token = stringToBase64.encode(rawString);
+
+                    final url = 'http://$ip/auth/login';
+
+                    try {
+                      final resp = await dio.post(
+                        url,
+                        options: Options(
+                          headers: {
+                            "authorization": "Basic $token",
+                          },
+                        ),
+                      );
+                      print(resp.data);
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: PRIMARY_COLOR,
                   ),
@@ -50,7 +97,26 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    String refreshToken =
+                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTcwMDgxNzg5NCwiZXhwIjoxNzAwOTA0Mjk0fQ.zirV978vg5uYhWmi2zrf_zKXz8fKBDwlTAbzAxQfvR8";
+
+                    final url = 'http://$ip/auth/token';
+
+                    try {
+                      final resp = await dio.post(
+                        url,
+                        options: Options(
+                          headers: {
+                            "authorization": "Bearer $refreshToken",
+                          },
+                        ),
+                      );
+                      print(resp.data);
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
                   style: TextButton.styleFrom(
                     foregroundColor: Colors.black,
                   ),
