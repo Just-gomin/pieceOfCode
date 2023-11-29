4405882aaff687515ccb7350fa9c22a52bff61af
안강민 <hkkm612@gmail.com>
2023-11-29 19:42:30 +0900

    update. [actual_flutter/restaurant] 음식점 목록 서버 데이터로 그리게 처리

--- pieceOfFlutter/actual_flutter/lib/restaurant/view/restaurant_screen.dart
+++ pieceOfFlutter/actual_flutter/lib/restaurant/view/restaurant_screen.dart
@@ -1,26 +1,64 @@
+import 'package:actual_flutter/common/const/data.dart';
 import 'package:actual_flutter/restaurant/component/restaurant_card.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
 
+  Future<List> paginateRestaurant() async {
+    final dio = Dio();
+
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get(
+      'http://$ip/restaurant',
+      options: Options(
+        headers: {'authorization': 'Bearer $accessToken'},
+      ),
+    );
+
+    return resp.data['data'];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Center(
-          child: RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이', '치즈', '매운맛'],
-            ratingCount: 100,
-            rating: 4.52,
-            deliveryTime: 15,
-            deliveryFee: 2000,
-          ),
-        ),
-      ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: FutureBuilder<List>(
+          future: paginateRestaurant(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Container();
+            } else {
+              return ListView.separated(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final item = snapshot.data![index];
+
+                  return Center(
+                    child: RestaurantCard(
+                      image: Image.network(
+                        'http://$ip${item['thumbUrl']}',
+                        fit: BoxFit.cover,
+                      ),
+                      name: item['name'],
+                      tags: List<String>.from(item['tags']),
+                      ratingsCount: item['ratingsCount'],
+                      ratings: item['ratings'],
+                      deliveryTime: item['deliveryTime'],
+                      deliveryFee: item['deliveryFee'],
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(
+                    height: 16.0,
+                  );
+                },
+              );
+            }
+          }),
     );
   }
 }
