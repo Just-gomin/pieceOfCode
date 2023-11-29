24a544c2bc94aea8fa83f36c4ccede31a571aeeb
안강민 <hkkm612@gmail.com>
2023-11-29 19:41:13 +0900

    refactor. [actual_flutter/app] 요청 IP 상수화

--- pieceOfFlutter/actual_flutter/lib/common/const/data.dart
+++ pieceOfFlutter/actual_flutter/lib/common/const/data.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
 const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN';
 
 final storage = FlutterSecureStorage();
+
+const emulatorIP = '10.0.0.2:3000';
+const simulatorIP = '127.0.0.1:3000';
+
+final ip = Platform.isAndroid ? emulatorIP : simulatorIP;
