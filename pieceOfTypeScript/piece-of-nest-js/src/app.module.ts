ebbf7211531359a4f5e6ea9fd6c988afd32a4176
안강민 <hkkm612@gmail.com>
2024-02-03 22:47:25 +0900

    refactor. [NestJS] cat & movie 모듈 제거

--- pieceOfTypeScript/piece-of-nest-js/src/app.module.ts
+++ pieceOfTypeScript/piece-of-nest-js/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [CatsModule, MoviesModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
