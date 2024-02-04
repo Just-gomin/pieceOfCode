0e809fd30e69cd795d17923bce2f67561f48e1e2
안강민 <hkkm612@gmail.com>
2024-02-04 11:20:10 +0900

    chore. [NestJS] 환경 변수 파일 설정에 주석 추가

--- pieceOfTypeScript/piece-of-nest-js/src/app.module.ts
+++ pieceOfTypeScript/piece-of-nest-js/src/app.module.ts
@@ -9,8 +9,8 @@ import { ConfigKey } from './core/config/enum/config-key.enum';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env[ConfigKey.nodeEnv]
-        ? `env/.${process.env[ConfigKey.nodeEnv]}.env`
-        : 'env/.local.env',
+        ? `env/.${process.env[ConfigKey.nodeEnv]}.env` // NODE_ENV 별 다른 파일
+        : 'env/.local.env', // NODE_ENV가 주어지지 않는 경우 .local.env로 처리
     }),
   ],
   controllers: [AppController],
