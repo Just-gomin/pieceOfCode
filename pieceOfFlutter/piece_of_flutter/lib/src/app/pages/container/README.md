e66406d6003703517037aa3943d1ee3bfc9926c0
안강민 <hkkm612@gmail.com>
2024-02-24 15:08:24 +0900

    feat: [Flutter] MarkDown 파일을 활용한 App Build

---
+++ pieceOfFlutter/piece_of_flutter/lib/src/app/pages/container/README.md
@@ -0,0 +1,5 @@
+# 💡 Container Widget
+
+## 📌 What is Contaienr?
+
+- 공식문서: <https://api.flutter.dev/flutter/widgets/Container-class.html>
--- pieceOfFlutter/piece_of_flutter/lib/src/app/pages/container/container_page.dart
+++ pieceOfFlutter/piece_of_flutter/lib/src/app/pages/container/container_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContainerPage extends StatelessWidget {
   static const String pageName = "Container";
 
   const ContainerPage({super.key});
+
+  Future<String> loadAsset() async {
+    var result =
+        await rootBundle.loadString('lib/src/app/pages/container/README.md');
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +20,28 @@ class ContainerPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Container"),
       ),
-      body: ListView(),
+      body: Center(
+        child: FutureBuilder(
+          future: loadAsset(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Markdown(
+                data: snapshot.data!,
+                selectable: true,
+                onTapLink: (text, href, title) {
+                  // print(text);
+                  if (href != null) {
+                    launchUrl(Uri.parse(href));
+                  }
+                  // print(title);
+                },
+              );
+            }
+
+            return const Text("No Data");
+          },
+        ),
+      ),
     );
   }
 }
