7c8dc945fc06e402ef7851d3610bf558df3b449f
Kangmin An <hkkm612@gmail.com>
2024-06-27 14:28:51 +0900

    feat(dart): init piece_of_test in pieceOfDart

---
+++ pieceOfDart/piece_of_test/bin/piece_of_test.dart
@@ -0,0 +1,5 @@
+import 'package:piece_of_test/piece_of_test.dart' as piece_of_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${piece_of_test.calculate()}!');
+}
---
+++ pieceOfDart/piece_of_test/test/piece_of_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:piece_of_test/piece_of_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
