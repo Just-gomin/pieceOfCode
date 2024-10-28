297cc2caa0a68a7c721763dfe7b3c90648c2f01c
Kangmin An <hkkm612@gmail.com>
2024-10-28 22:43:45 +0900

    feat(java): spring-data basic

--- pieceOfSpringBoot/src/main/java/com/example/pieceOfSpringBoot/PieceOfSpringBootApplication.java
+++ pieceOfSpringBoot/src/main/java/com/example/pieceOfSpringBoot/PieceOfSpringBootApplication.java
@@ -1,214 +1,214 @@
-package com.example.pieceOfSpringBoot;
+ package com.example.pieceOfSpringBoot;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+ import java.util.List;
+ import java.util.Optional;
+ import java.util.UUID;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+ import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.boot.context.properties.ConfigurationProperties;
+ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.data.repository.CrudRepository;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.stereotype.Component;
+ import org.springframework.web.bind.annotation.DeleteMapping;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PathVariable;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.PutMapping;
+ import org.springframework.web.bind.annotation.RequestBody;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RequestMethod;
+ import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+ import jakarta.annotation.PostConstruct;
+ import jakarta.persistence.Entity;
+ import jakarta.persistence.Id;
 
-@SpringBootApplication
-@ConfigurationPropertiesScan
-public class PieceOfSpringBootApplication {
+ @SpringBootApplication
+ @ConfigurationPropertiesScan
+ public class PieceOfSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PieceOfSpringBootApplication.class, args);
-	}
+ 	public static void main(String[] args) {
+ 		SpringApplication.run(PieceOfSpringBootApplication.class, args);
+ 	}
 
-	@Bean
-	@ConfigurationProperties(prefix = "droid")
-	Droid createDroid() {
-		return new Droid();
-	}
-}
+ 	@Bean
+ 	@ConfigurationProperties(prefix = "droid")
+ 	Droid createDroid() {
+ 		return new Droid();
+ 	}
+ }
 
-@Entity
-class Coffee {
-	// 특정 커피 종류의 고유값
-	@Id
-	private String id;
+ @Entity
+ class Coffee {
+ 	// 특정 커피 종류의 고유값
+ 	@Id
+ 	private String id;
 
-	// 커피(종류)명
-	private String name;
+ 	// 커피(종류)명
+ 	private String name;
 
-	public Coffee() {
-	}
+ 	public Coffee() {
+ 	}
 
-	public Coffee(String id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+ 	public Coffee(String id, String name) {
+ 		this.id = id;
+ 		this.name = name;
+ 	}
 
-	public Coffee(String name) {
-		this(UUID.randomUUID().toString(), name);
-	}
+ 	public Coffee(String name) {
+ 		this(UUID.randomUUID().toString(), name);
+ 	}
 
-	public String getId() {
-		return this.id;
-	}
+ 	public String getId() {
+ 		return this.id;
+ 	}
 
-	public String getName() {
-		return this.name;
-	}
+ 	public String getName() {
+ 		return this.name;
+ 	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
+ 	public void setId(String id) {
+ 		this.id = id;
+ 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-}
+ 	public void setName(String name) {
+ 		this.name = name;
+ 	}
+ }
 
-interface CoffeeRepository extends CrudRepository<Coffee, String> {
-}
+ interface CoffeeRepository extends CrudRepository<Coffee, String> {
+ }
 
-@Component
-class DataLoader {
-	private final CoffeeRepository coffeeRepository;
+ @Component
+ class DataLoader {
+ 	private final CoffeeRepository coffeeRepository;
 
-	public DataLoader(CoffeeRepository coffeeRepository) {
-		this.coffeeRepository = coffeeRepository;
-	}
+ 	public DataLoader(CoffeeRepository coffeeRepository) {
+ 		this.coffeeRepository = coffeeRepository;
+ 	}
 
-	@PostConstruct
-	private void loadData() {
-		coffeeRepository.saveAll(List.of(
-				new Coffee("Cage Cereza"),
-				new Coffee("Cage Ganador"),
-				new Coffee("Cage Lareno"),
-				new Coffee("Cage Tres Pontas")));
-	}
-}
+ 	@PostConstruct
+ 	private void loadData() {
+ 		coffeeRepository.saveAll(List.of(
+ 				new Coffee("Cage Cereza"),
+ 				new Coffee("Cage Ganador"),
+ 				new Coffee("Cage Lareno"),
+ 				new Coffee("Cage Tres Pontas")));
+ 	}
+ }
 
-@RestController
-class RestApiDemoController {
-	private final CoffeeRepository coffeeRepository;
+ @RestController
+ class RestApiDemoController {
+ 	private final CoffeeRepository coffeeRepository;
 
-	public RestApiDemoController(CoffeeRepository coffeeRepository) {
-		this.coffeeRepository = coffeeRepository;
-	}
+ 	public RestApiDemoController(CoffeeRepository coffeeRepository) {
+ 		this.coffeeRepository = coffeeRepository;
+ 	}
 
-	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
-	Iterable<Coffee> getCoffees() {
-		return coffeeRepository.findAll();
-	}
+ 	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
+ 	Iterable<Coffee> getCoffees() {
+ 		return coffeeRepository.findAll();
+ 	}
 
-	@GetMapping("/coffees/{id}")
-	Optional<Coffee> getCoffeeById(@PathVariable String id) {
-		return coffeeRepository.findById(id);
-	}
+ 	@GetMapping("/coffees/{id}")
+ 	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+ 		return coffeeRepository.findById(id);
+ 	}
 
-	@PostMapping("/coffeess")
-	Coffee postCoffee(@RequestBody Coffee coffee) {
-		return coffeeRepository.save(coffee);
-	}
+ 	@PostMapping("/coffeess")
+ 	Coffee postCoffee(@RequestBody Coffee coffee) {
+ 		return coffeeRepository.save(coffee);
+ 	}
 
-	@PutMapping("/coffees/{id}")
-	ResponseEntity<Coffee> putCoffeeById(@PathVariable String id, @RequestBody Coffee coffee) {
-		return new ResponseEntity<>(coffeeRepository.save(coffee),
-				coffeeRepository.existsById(id) ? HttpStatus.OK : HttpStatus.CREATED);
-	}
+ 	@PutMapping("/coffees/{id}")
+ 	ResponseEntity<Coffee> putCoffeeById(@PathVariable String id, @RequestBody Coffee coffee) {
+ 		return new ResponseEntity<>(coffeeRepository.save(coffee),
+ 				coffeeRepository.existsById(id) ? HttpStatus.OK : HttpStatus.CREATED);
+ 	}
 
-	@DeleteMapping("/coffees/{id}")
-	void deleteCoffeeById(@PathVariable String id) {
-		coffeeRepository.deleteById(id);
-	}
-}
+ 	@DeleteMapping("/coffees/{id}")
+ 	void deleteCoffeeById(@PathVariable String id) {
+ 		coffeeRepository.deleteById(id);
+ 	}
+ }
 
-@ConfigurationProperties(prefix = "greeting")
-class Greeting {
-	private String name;
-	private String coffee;
+ @ConfigurationProperties(prefix = "greeting")
+ class Greeting {
+ 	private String name;
+ 	private String coffee;
 
-	public String getName() {
-		return name;
-	}
+ 	public String getName() {
+ 		return name;
+ 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+ 	public void setName(String name) {
+ 		this.name = name;
+ 	}
 
-	public String getCoffee() {
-		return coffee;
-	}
+ 	public String getCoffee() {
+ 		return coffee;
+ 	}
 
-	public void setCoffee(String coffee) {
-		this.coffee = coffee;
-	}
-}
+ 	public void setCoffee(String coffee) {
+ 		this.coffee = coffee;
+ 	}
+ }
 
-@RestController
-@RequestMapping("/greeting")
-class GreetingController {
-	private final Greeting greeting;
+ @RestController
+ @RequestMapping("/greeting")
+ class GreetingController {
+ 	private final Greeting greeting;
 
-	public GreetingController(Greeting greeting) {
-		this.greeting = greeting;
-	}
+ 	public GreetingController(Greeting greeting) {
+ 		this.greeting = greeting;
+ 	}
 
-	@GetMapping
-	String getGreeting() {
-		return greeting.getName();
-	}
+ 	@GetMapping
+ 	String getGreeting() {
+ 		return greeting.getName();
+ 	}
 
-	@GetMapping("/coffee")
-	String getNameAndCoffee() {
-		return greeting.getCoffee();
-	}
-}
+ 	@GetMapping("/coffee")
+ 	String getNameAndCoffee() {
+ 		return greeting.getCoffee();
+ 	}
+ }
 
-class Droid {
-	private String id, description;
+ class Droid {
+ 	private String id, description;
 
-	public String getId() {
-		return id;
-	}
+ 	public String getId() {
+ 		return id;
+ 	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
+ 	public void setId(String id) {
+ 		this.id = id;
+ 	}
 
-	public String getDescription() {
-		return this.description;
-	}
+ 	public String getDescription() {
+ 		return this.description;
+ 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-}
+ 	public void setDescription(String description) {
+ 		this.description = description;
+ 	}
+ }
 
-@RestController
-@RequestMapping("/droid")
-class DroidController {
-	private final Droid droid;
+ @RestController
+ @RequestMapping("/droid")
+ class DroidController {
+ 	private final Droid droid;
 
-	public DroidController(Droid droid) {
-		this.droid = droid;
-	}
+ 	public DroidController(Droid droid) {
+ 		this.droid = droid;
+ 	}
 
-	@GetMapping
-	Droid getDroid() {
-		return droid;
-	}
-}
+ 	@GetMapping
+ 	Droid getDroid() {
+ 		return droid;
+ 	}
+ }
