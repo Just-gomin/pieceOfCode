367d6cc671814d002bc25602342af86dd1aae580
Kangmin An <hkkm612@gmail.com>
2024-11-10 17:08:04 +0900

    feat(flutter-animation): add ColorTween to TweenPage

--- pieceOfFlutter/piece_of_flutter_animation/lib/src/pages/tween_page.dart
+++ pieceOfFlutter/piece_of_flutter_animation/lib/src/pages/tween_page.dart
@@ -23,9 +23,39 @@ class _BuildBody extends StatefulWidget {
   State<_BuildBody> createState() => __BuildBodyState();
 }
 
-class __BuildBodyState extends State<_BuildBody> {
+class __BuildBodyState extends State<_BuildBody> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
+    _colorAnimation = ColorTween(
+      begin: Colors.red,
+      end: Colors.yellow,
+    ).animate(_controller);
+
+    // _controller.repeat(); // 반복할 때, end에서 바로 begin으로 초기화 하여 반복
+    _controller.repeat(reverse: true); // 반복할 때, end에서 begin으로 돌아가며 반복
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: AnimatedBuilder(
+        animation: _colorAnimation,
+        builder: (context, child) {
+          return Container(
+            width: 200,
+            height: 200,
+            color: _colorAnimation.value,
+          );
+        },
+      ),
+    );
   }
 }
