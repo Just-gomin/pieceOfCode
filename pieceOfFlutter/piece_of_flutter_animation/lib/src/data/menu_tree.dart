147feb53ddb04dfde62317c924f093a27a54883c
Kangmin An <hkkm612@gmail.com>
2024-11-10 17:40:56 +0900

    feat(flutter-animation): add menu tree

--- pieceOfFlutter/piece_of_flutter_animation/lib/src/data/menu_tree.dart
+++ pieceOfFlutter/piece_of_flutter_animation/lib/src/data/menu_tree.dart
@@ -1,11 +1,16 @@
 import 'package:piece_of_flutter_animation/src/domain/models/models.dart';
 
 const Menu animationBase = Menu(
-  title: 'Animation 개념',
+  title: 'Animation Concepts',
   subMenu: [
     Menu(
       title: 'Tween(트윈)',
       path: '/tween',
     ),
   ],
+);
+
+const Menu animationSamples = Menu(
+  title: 'Animation Samples',
+  subMenu: [],
 );
