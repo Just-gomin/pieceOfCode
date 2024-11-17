7878cc7402a0717ec5111770ac6316a2ac20b420
Kangmin An <hkkm612@gmail.com>
2024-11-17 22:41:11 +0900

    feat(Spring-Boot): add WebSocket connection

--- pieceOfSpringBoot/pieceOfTemplate/src/main/java/com/position/aircrafts/pieceOfTemplate/PositionRetriever.java
+++ pieceOfSpringBoot/pieceOfTemplate/src/main/java/com/position/aircrafts/pieceOfTemplate/PositionRetriever.java
@@ -3,7 +3,10 @@ package com.position.aircrafts.pieceOfTemplate;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -11,13 +14,28 @@ import java.util.function.Consumer;
 @Configuration
 public class PositionRetriever {
     private final AircraftRepository repo;
+    private final WebSocketHandler handler;
 
     @Bean
     Consumer<List<Aircraft>> retrieveAircraftPositions() {
         return acList -> {
 //            repo.deleteAll();
             repo.saveAll(acList);
-            repo.findAll().forEach(System.out::println);
+            sendPostitions();
         };
+    }
+
+    private void sendPostitions() {
+        if (repo.count() > 0) {
+            for (WebSocketSession sessionInList : handler.getSessionList()) {
+                try {
+                    sessionInList.sendMessage(
+                            new TextMessage(repo.findAll().toString())
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
