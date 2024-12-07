4b21a569ee8078cedb05cdb5bb620ecaac47bf2c
Kangmin An <hkkm612@gmail.com>
2024-12-07 14:46:11 +0900

    feat(flutter-animation): add text color control when hover

--- pieceOfFlutter/piece_of_flutter_animation/lib/src/pages/home_page.dart
+++ pieceOfFlutter/piece_of_flutter_animation/lib/src/pages/home_page.dart
@@ -49,11 +49,12 @@ class _BuildBody extends StatelessWidget {
 }
 
 class _BuildSectionWithItems extends StatelessWidget {
-  const _BuildSectionWithItems({
+  _BuildSectionWithItems({
     required this.menu,
   });
 
   final Menu menu;
+  final FocusNode focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +62,9 @@ class _BuildSectionWithItems extends StatelessWidget {
         delegate: SliverChildListDelegate(
       [
         _BuildSectionTitle(title: menu.title),
-        ...(menu.subMenu ?? []).map((item) => _BuildListItem(menu: item))
+        ...(menu.subMenu ?? []).map(
+          (item) => _BuildListItem(menu: item),
+        )
       ],
     ));
   }
@@ -98,26 +101,53 @@ class _BuildSectionTitle extends StatelessWidget {
   }
 }
 
-class _BuildListItem extends StatelessWidget {
+class _BuildListItem extends StatefulWidget {
   const _BuildListItem({required this.menu});
 
   final Menu menu;
 
   @override
+  State<_BuildListItem> createState() => _BuildListItemState();
+}
+
+class _BuildListItemState extends State<_BuildListItem> {
+  bool isHover = false;
+
+  void setHoverState(bool val) {
+    setState(() {
+      isHover = val;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListTile(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-      ),
-      title: Text(
-        menu.title,
-        style: const TextStyle(fontWeight: FontWeight.w500),
-      ),
-      onTap: () {
-        context.push(menu.path!);
+    return MouseRegion(
+      onHover: (event) {
+        setHoverState(true);
       },
-      hoverColor: Theme.of(context).primaryColor,
-      splashColor: Theme.of(context).primaryColor.withAlpha(128),
+      onExit: (event) {
+        setHoverState(false);
+      },
+      onEnter: (event) {
+        setHoverState(true);
+      },
+      child: ListTile(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        ),
+        title: Text(
+          widget.menu.title,
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: isHover ? Colors.white : Colors.black,
+          ),
+        ),
+        onTap: () {
+          context.push(widget.menu.path!);
+        },
+        hoverColor: Theme.of(context).primaryColor,
+        splashColor: Theme.of(context).primaryColor.withAlpha(128),
+      ),
     );
   }
 }
