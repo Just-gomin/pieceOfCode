f0649440f09087d89c1f805aafd85f8fafc807f9
Kangmin An <hkkm612@gmail.com>
2025-05-19 11:14:34 +0900

    feat(dart): verify that Set preserves the input order.

--- pieceOfDart/piece_of_dart/lib/set_pad.dart
+++ pieceOfDart/piece_of_dart/lib/set_pad.dart
@@ -1,9 +1,18 @@
 void main() {
-  Set<String> stringSet = <String>{'a', 'b', 'c', 'd', 'e'};
+  Set<String> stringSet = <String>{};
+
+  stringSet.add('a');
   print(stringSet);
+  stringSet.add('d');
+  stringSet.add('b');
+  print(stringSet);
+  stringSet.add('c');
+  stringSet.add('e');
+  print(stringSet);
+  stringSet.add('c');
+  print(stringSet); // Set 은 입력 순서가 유지 된다.
 
   int indexOfA = stringSet.toList().indexWhere((i) => i == 'a');
   int indexOfB = stringSet.toList().indexWhere((i) => i == 'b');
-
   print('$indexOfA, $indexOfB');
 }
